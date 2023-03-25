9abe8a3b26919fc7780f397579b1424be4b8f38f
Picasso004 <sanvee34@gmail.com>
2023-03-26 00:35:19 +0100

    Update

--- backup_extractor.py
+++ backup_extractor.py
@@ -11,11 +11,11 @@ class BackupExtractor:
         metadata ={}
         manifest = os.path.join(self.backup_path, "Manifest.plist")
         with open(manifest, 'rb') as fp:
-            metadata = plistlib.load(fp)
+            metadata['Manifest'] = plistlib.load(fp)
 
         info = os.path.join(self.backup_path, "Info.plist")
         with open(info, 'rb') as fp:
-            metadata.update(plistlib.load(fp))
+            metadata['Info'] = plistlib.load(fp)
 
         status = os.path.join(self.backup_path, "Status.plist")
         with open(status, 'rb') as fp:
@@ -24,5 +24,4 @@ class BackupExtractor:
 
     def extract_data(self):
         """extracts data from the backup file and returns it as a dictionary"""
-
         return self.extracted_data
