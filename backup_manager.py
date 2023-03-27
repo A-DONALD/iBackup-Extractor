055852608c3e5d316411f80ccee7038001d663c8
Y-DONALD <donaldyoungoua@gmail.com>
2023-03-27 14:11:27 +0200

    essai d'apprentissage

--- backup_manager.py
+++ backup_manager.py
@@ -1,3 +1,6 @@
 
 class BackupManager:
     """This class is responsible for managing the backup files and directories."""
+    total = 1
+    total = total +1
+    print("la valeur de total est "+total)
