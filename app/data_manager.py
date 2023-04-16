3133c574df6d9b2d9c3f16d4ea1fe174c5587040
Y-DONALD <donaldyoungoua@gmail.com>
2023-04-16 16:02:07 +0200

    try how to convert information to CSV

--- app/data_manager.py
+++ app/data_manager.py
@@ -1,3 +1,10 @@
 
 class DataManager:
     """This class is responsible for managing the extracted data, storing it, and retrieving it as necessary."""
+    import csv
+    with open(r'C:\Users\donal\Downloads\backup samples\test.csv', 'w', newline='') as fp:
+        a = csv.writer(fp, delimiter=';')
+        data = [['Me', 'You'],
+                ['293', '219'],
+                ['54', '13']]
+        a.writerows(data)
