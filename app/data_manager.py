93844ec99b195e8111bc5f1870d65adf2efa2496
Y-DONALD <donaldyoungoua@gmail.com>
2023-04-17 14:50:41 +0200

    try how to convert dictionary to CSV

--- app/data_manager.py
+++ app/data_manager.py
@@ -1,10 +1,16 @@
+import csv
 
-class DataManager:
-    """This class is responsible for managing the extracted data, storing it, and retrieving it as necessary."""
-    import csv
-    with open(r'C:\Users\donal\Downloads\backup samples\test.csv', 'w', newline='') as fp:
-        a = csv.writer(fp, delimiter=';')
-        data = [['Me', 'You'],
-                ['293', '219'],
-                ['54', '13']]
-        a.writerows(data)
+employee_info = ['emp_id', 'emp_name', 'skills']
+
+new_dict = [
+    {'emp_id': 456, 'emp_name': 'George', 'skills': 'Python'},
+    {'emp_id': 892, 'emp_name': 'Adam', 'skills': 'Java'},
+    {'emp_id': 178, 'emp_name': 'Gilchrist', 'skills': 'Mongo db'},
+    {'emp_id': 155, 'emp_name': 'Elon', 'skills': 'Sql'},
+    {'emp_id': 299, 'emp_name': 'Mask', 'skills': 'Ruby'},
+]
+
+with open(r'C:\Users\donal\Downloads\backup samples\test.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=employee_info, delimiter=';')
+    writer.writeheader()
+    writer.writerows(new_dict)
