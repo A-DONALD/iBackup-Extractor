ea55222ec3cf31bdd689a9bcf5242e148eb7a5e1
Y-DONALD <donaldyoungoua@gmail.com>
2023-04-21 15:39:32 +0200

    (warn) - major_change - remove extracted_data as parameter

--- app/data_manager.py
+++ app/data_manager.py
@@ -4,20 +4,19 @@ class DataManager:
     """This class is responsible for managing the extracted data, storing it, and retrieving it as necessary."""
     def __init__(self, data_dir):
         self.__data_dir = data_dir
-        self.__extracted_data = dict()
 
-    def export_contacts(self):
+    def export_contacts(self, extracted_data):
         with open(os.path.join(self.__data_dir, "contacts.csv"), 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, self.__extracted_data.keys(), delimiter=';')
+            writer = csv.DictWriter(csvfile, extracted_data.keys(), delimiter=';')
             writer.writeheader()
-            writer.writerows(self.__extracted_data)
-    def export_call_history(self):
+            writer.writerows(extracted_data)
+    def export_call_history(self, extracted_data):
         with open(os.path.join(self.__data_dir, "call_history.csv"), 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, self.__extracted_data.keys(), delimiter=';')
+            writer = csv.DictWriter(csvfile, extracted_data.keys(), delimiter=';')
             writer.writeheader()
-            writer.writerows(self.__extracted_data)
-    def export_web_history(self):
+            writer.writerows(extracted_data)
+    def export_web_history(self, extracted_data):
         with open(os.path.join(self.__data_dir, "web_history.csv"), 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, self.__extracted_data.keys(), delimiter=';')
+            writer = csv.DictWriter(csvfile, extracted_data.keys(), delimiter=';')
             writer.writeheader()
-            writer.writerows(self.__extracted_data)
+            writer.writerows(extracted_data)
